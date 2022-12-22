21b7e51141edef05379700572fd16c2584c54609
Maryam (Maryamalsadat) Tabatabaei <87692864+maryamsadat-tabatabaei@users.noreply.github.com>
2022-12-22 13:45:40 +0330

    Fix some bugs

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,5 +1,5 @@
 const { validationResult } = require("express-validator");
-const bcrypt = require("bcrypt.js");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const HttpError = require("../models/http-error");
 const User = require("../models/user");
